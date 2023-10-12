ab82a72148317730b222d69a0984b73733048daa
Amadou <amadoucoulibaly618@gmail.com>
2023-10-12 20:49:28 +0200

    Plugin import extensions return type introduction

--- ChainedFunctions/Extensions/KernelExtensions.cs
+++ ChainedFunctions/Extensions/KernelExtensions.cs
@@ -1,12 +1,11 @@
 using ChainedFunctions.Skills;
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.SkillDefinition;
 
 namespace ChainedFunctions.Extensions;
 
 public static class KernelExtensions
 {
-    public static void ImportExtractJsonPlugin(this IKernel kernel)
-    {
-        kernel.ImportSkill(new ExtractJson(), nameof(ExtractJson));
-    }
+    public static IDictionary<string, ISKFunction> ImportExtractJsonPlugin(this IKernel kernel)
+        => kernel.ImportSkill(new ExtractJson(), nameof(ExtractJson));
 }
--- WeatherPlugin/Extensions/KernelExtensions.cs
+++ WeatherPlugin/Extensions/KernelExtensions.cs
@@ -1,12 +1,11 @@
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.SkillDefinition;
 using WeatherPlugin.Plugins.Weather;
 
 namespace WeatherPlugin.Extensions;
 
 public static class KernelExtensions
 {
-    public static void ImportWeatherPlugin(this IKernel kernel, string apiKey)
-    {
-        kernel.ImportSkill(new SkWeatherPlugin(apiKey), nameof(SkWeatherPlugin));
-    }
+    public static IDictionary<string, ISKFunction> ImportWeatherPlugin(this IKernel kernel, string apiKey)
+        => kernel.ImportSkill(new SkWeatherPlugin(apiKey), nameof(SkWeatherPlugin));
 }
